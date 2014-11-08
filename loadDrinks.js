b71499dfdcd960aea51c214cabec9b7b62f64f9f
Giovanni Charles <giovanni.charles@gmail.com>
2014-11-08 21:44:39 +0000

    test drinks

--- loadDrinks.js
+++ loadDrinks.js
@@ -1,43 +1,17 @@
 var mongoose = require('mongoose');
 var drinks = require('./wetherspoons.json');
-mongoose.connect('mongodb://localhost/goodnight');
-var Place = require('./server/api/place/place.model.js')
+mongoose.connect('mongodb://localhost/goodnight-dev');
 var Drink = require('./server/api/drink/drink.model.js')
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
-  Place.find({name: 'Wetherspoons'}, function(err, ws){
+  Drink.collection.insert(drinks, function(err, docs){
     if(err){
       console.log(err);
+      mongoose.disconnect();
       return;
     }
-    console.log(ws);
-    //var drinkslist = drinks.map(function(d){
-      //return {
-        //units: d.units,
-        //price: d.price,
-        //img: d.img
-      //};
-    //});
-
-    //function uniq(a) {
-        //var seen = {};
-        //var list = a.filter(function(item) {
-            //return seen.hasOwnProperty(item.name) ? false : (seen[item.name] = true);
-        //});
-        //console.log(seen);
-        //return list;
-    //}
-
-    //console.log(drinkslist);
-    Drink.collection.insert(drinks, function(err, docs){
-      if(err){
-        console.log(err);
-        mongoose.disconnect();
-        return;
-      }
-      console.log(docs.length + ' drinks inserted')
-      mongoose.disconnect();
-    })
-  });
+    console.log(docs.length + ' drinks inserted')
+    mongoose.disconnect();
+  })
 })
