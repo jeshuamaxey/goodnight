7e4c98c7f99a52ead7469c156159d194045fcc06
Giovanni Charles <giovanni.charles@gmail.com>
2014-11-08 20:36:32 +0000

    more places

--- load.js
+++ load.js
@@ -1,17 +1,18 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/goodnight');
+mongoose.connect('mongodb://localhost/goodnight-dev');
 var Place = require('./server/api/place/place.model.js')
+var places = require('./places.json');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
-  var ws = new Place({name: 'Wetherspoons'});
-  ws.save(function(err, doc){
-    if(err){
-      console.error(err);
-    }
-    console.log(doc);
-    mongoose.disconnect();
-    }
-  );
+  Place.collection.insert(places, function(err, docs){
+      if(err){
+        console.trace(err);
+        mongoose.disconnect();
+        return;
+      }
+      console.log(docs.length + ' inserted')
+      mongoose.disconnect();
+  });
 });
