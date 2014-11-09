255740b001b5c25b54102b1a8c165cf97b790b33
James McAulay <hello@jamesmcaulay.co.uk>
2014-11-09 14:52:40 +0000

    Navbar fixed

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,15 +3,17 @@
 angular.module('goodnightApp')
   .controller('MainCtrl', function ($scope, $http, User, Auth) {
     // 
-    var user = User.get()
+    var user = User.get();
+    var currentUser = Auth.getCurrentUser();
     console.log(user);
     console.log($scope.user)
 
     $scope.activeQuest = true;
     if (Auth.getCurrentUser() != 'undefined') {
+        $scope.moneySpent = Auth.getCurrentUser().summary.moneySpent.toFixed(2);
     	$scope.loggedIn = true;
+        $scope.unitsConsumed = Auth.getCurrentUser().summary.unitsConsumed.toFixed(2);
     } else {
     	$scope.loggedIn = false;
     }
-    console.log(Auth.getCurrentUser());
   });
